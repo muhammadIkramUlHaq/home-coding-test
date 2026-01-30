afcee2bbad40e2338e99348cf3d3aff6627a11ca
muhammadIkramUlHaq <ikramulhaq1992@gmail.com>
2026-01-30 17:34:55 +0100

    feat(backend): add health endpoint

---
+++ backend/src/test/java/dev/mikram/homeassignment/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikram.homeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
